43bdbaf09f6939ab0082b32a4abbc90d1ac9a3a5
mdmmn378 <mdmmn378@gmail.com>
2022-12-07 01:00:50 +0600

    Upto struct

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(units: i32) -> i32 {
+    if units > 40 {
+        return units;
+    } else {
+        return units * 2;
+    }
+}
 
 // Don't modify this function!
 #[test]

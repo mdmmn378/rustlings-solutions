e01d9c93efdfdedc32253f4cf86cc88b4888fe37
mdmmn378 <mdmmn378@gmail.com>
2022-12-09 18:38:25 +0600

    Upto quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,33 +16,36 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: Grade,
+// quiz3.rs
+// This quiz tests:
+// - Generics
+// - Traits
+// An imaginary magical school has a new report card generation system written in Rust!
+// Currently the system only supports creating report cards where the student's grade
+// is represented numerically (e.g. 1.0 -> 5.5).
+// However, the school also issues alphabetical grades (A+ -> F-) and needs
+// to be able to print both types of report card!
+
+// Make the necessary code changes in the struct ReportCard and the impl block
+// to support alphabetical report cards. Change the Grade in the second test to "A+"
+// to show that your changes allow alphabetical grades.
+
+// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+
+// I AM NOT DONE
+use std::fmt::Display;
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-pub enum Grade {
-    Number(f32),
-    Letter(String),
-}
-
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
-        match &self.grade {
-            Grade::Number(i) => {
-                format!(
-                    "{} ({}) - achieved a grade of {}",
-                    &self.student_name, &self.student_age, i
-                )
-            }
-            Grade::Letter(i) => {
-                format!(
-                    "{} ({}) - achieved a grade of {}",
-                    &self.student_name, &self.student_age, i
-                )
-            }
-        }
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -53,7 +56,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: Grade::Number(2.1),
+            grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -67,7 +70,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: Grade::Letter("A+".to_string()),
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
@@ -77,3 +80,65 @@ mod tests {
         );
     }
 }
+
+// pub struct ReportCard {
+//     pub grade: Grade,
+//     pub student_name: String,
+//     pub student_age: u8,
+// }
+
+// pub enum Grade {
+//     Number(f32),
+//     Letter(String),
+// }
+
+// impl ReportCard {
+//     pub fn print(&self) -> String {
+//         match &self.grade {
+//             Grade::Number(i) => {
+//                 format!(
+//                     "{} ({}) - achieved a grade of {}",
+//                     &self.student_name, &self.student_age, i
+//                 )
+//             }
+//             Grade::Letter(i) => {
+//                 format!(
+//                     "{} ({}) - achieved a grade of {}",
+//                     &self.student_name, &self.student_age, i
+//                 )
+//             }
+//         }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn generate_numeric_report_card() {
+//         let report_card = ReportCard {
+//             grade: Grade::Number(2.1),
+//             student_name: "Tom Wriggle".to_string(),
+//             student_age: 12,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Tom Wriggle (12) - achieved a grade of 2.1"
+//         );
+//     }
+
+//     #[test]
+//     fn generate_alphabetic_report_card() {
+//         // TODO: Make sure to change the grade here after you finish the exercise.
+//         let report_card = ReportCard {
+//             grade: Grade::Letter("A+".to_string()),
+//             student_name: "Gary Plotter".to_string(),
+//             student_age: 11,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Gary Plotter (11) - achieved a grade of A+"
+//         );
+//     }
+// }
